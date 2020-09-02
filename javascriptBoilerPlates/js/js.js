e6fb3459f846c62796042289824be5cd5d6ba602
Hamid <54853635+hamiboy8260@users.noreply.github.com>
2020-09-02 13:18:17 +0200

    Add files via upload

---
+++ javascriptBoilerPlates/js/js.js
@@ -0,0 +1,32 @@
+/**
+ * file: js.js
+ * purpose:mange firkanter og skabe elementer via createelement
+ **/
+console.log('JavaScript from js/js.js: up and running!');
+
+let Many = function(howMany){
+
+  let h = document.body.clientHeight;
+  let w = document.body.clientWidth;
+
+	for(let i=0; i<howMany; i++){
+  
+  	// create elements on the fly
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("red");
+    newDiv.style.left = Math.floor( Math.random() * w) + 'px';
+    newDiv.style.top = Math.floor( Math.random() * h) + 'px';
+    newDiv.style.transform = 'rotate('
+    + Math.floor( Math.random()*360) 
+    + 'deg)';
+
+		scene.appendChild(newDiv);
+		console.log('Many exec...'); // test
+  }
+}
+
+klik.addEventListener("onclick", Many(67));
+
+//document.getElementById("klik").onclick= Many(Math.floor(Math.random() * 3 ) + 1)
+
+//Many(Math.floor(Math.random() * 10) + 1);
